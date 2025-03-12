d6d10ebc30cf6eff4f2d1b062e8bf6ccaed0cb84
Gonzalo Cárdenas <lalo020404@gmail.com>
2025-03-12 09:01:02 -0600

    Se añaden los cambios de la clase del 12 de Marzo del 2025

---
+++ ContactosApp/app/src/main/java/com/example/contactosapp/ContactStore.java
@@ -0,0 +1,41 @@
+package com.example.contactosapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactStore {
+    List<Contact> cache = new ArrayList<>();
+
+    public ContactStore() {
+        this.cache = ContactStore.createContacts();
+    }
+
+    public List<Contact> getCache() {
+        return cache;
+    }
+
+    public Contact get(int position) {
+        return cache.get(position);
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    public Contact findContactByPhone(String phoneNumber) {
+        for (Contact c: cache) {
+            if (c.phone.equals(phoneNumber)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public static List<Contact> createContacts() {
+        List<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Ariana Grande", "1553853365"));
+        contacts.add(new Contact("Kenia Os", "1553853365"));
+        contacts.add(new Contact("Cornejo Cornejo", "1553853365"));
+        return contacts;
+    }
+}
